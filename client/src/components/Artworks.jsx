59f572152f45208b1b6cffaf65852259786d353f
Paula Pniewska <pniewska.paula@gmail.com>
2024-12-03 14:36:33 +0100

    add no artworks found page and data file

--- client/src/components/Artworks.jsx
+++ client/src/components/Artworks.jsx
@@ -1,63 +1,19 @@
 import React, { useState } from 'react'
 import Artwork from '../pages/gallery/Artwork'
 
-import Thumb1 from '../assets/artwork-1.jpg';
-import Thumb2 from '../assets/artwork-2.jpg';
-import Thumb3 from '../assets/artwork-3.jpg';
-
-const artworks = [
-    {
-        id: '1',
-        thumb: Thumb1,
-        category: 'painting',
-        title: 'title1',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        artistID: 1,
-    },
-    {
-        id: '2',
-        thumb: Thumb2,
-        category: 'sculpture',
-        title: 'title2',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        artistID: 2,
-    },
-    {
-        id: '3',
-        thumb: Thumb3,
-        category: 'video',
-        title: 'title3',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        artistID: 3,
-    },
-    {
-        id: '4',
-        thumb: Thumb3,
-        category: 'video',
-        title: 'title3',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        artistID: 3,
-    },
-    {
-        id: '5',
-        thumb: Thumb1,
-        category: 'video',
-        title: 'title3',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        artistID: 1,
-    },
-]
-
+import data from '../data'
 
 const Artworks = () => {
-    const [artwork, setArtwork] = useState(artworks);
+    const [artworks, setArtworks] = useState(data);
 
     return (
-        <div className='container artworks'>
-            {
-                artworks.map(({id, thumb, category, title, desc, authorID}) => 
-                    <Artwork key={id} artworkID={id} thumb={thumb} category={category} title={title} desc={desc} authorID={authorID} />)
-            }
+        <div>
+            {artworks.length > 0 ? <div className='container artworks'>
+                {
+                    artworks.map(({id, thumb, category, title, desc, authorID}) => 
+                        <Artwork key={id} artworkID={id} thumb={thumb} category={category} title={title} desc={desc} authorID={authorID} />)
+                }
+            </div> : <h2 className='container'>No artworks found</h2>}
         </div>
     )
     }
